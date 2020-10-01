91f4cdb49add07950df513d295a227940d311841
KoliverdovLS <koliverdovls@sgu.ru>
2020-10-01 23:23:38 +0400

    feat: complet task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    let minimal = array[0];
+    for (i = 0; i < array.length; i++) {
+        if (array[i] < minimal) {
+            minimal = array[i];
+        }
+    }
+    return minimal;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    let maximal = array[0];
+    for (i = 0; i < array.length; i++) {
+        if (array[i] > maximal) {
+            maximal = array[i];
+        }
+    }
+    return maximal;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0) {
+        return 0;
+    }
+
+    let avg = 0;
+
+    for (i = 0; i < array.length; i++) {
+        avg += array[i];
+    }
+
+    return avg/array.length;
 }
